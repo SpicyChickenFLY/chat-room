af1f83d17878328162b6bcccabdafff761eff1cf
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-11-27 17:55:06 +0800

    修复问题

--- back/app/routes/user.py
+++ back/app/routes/user.py
@@ -1,11 +1,10 @@
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token
 from werkzeug import security
 
 from app import services
 from app import entities
-from app.entities.result import Result
+
 
 class UsersApi(Resource):
 
@@ -15,27 +14,23 @@ class UsersApi(Resource):
         username = data["username"]
         password = data["password"]
         if not username or not password:
-            return Result(msg="用户名密码不能为空").to_dict(), 400
+            return {"msg": "用户名密码不能为空"}, 400
         user = services.find_user_by_nickname(username)
         if user is not None:
-            return Result(msg="用户名已存在").to_dict(), 400
+            return {"msg": "用户名已存在"}, 400
 
-        user = entities.User(
-            nickname=username,
-            token=security.generate_password_hash(password)
-        )
+        user = entities.User()
+        user.nickname = username
+        user.token = security.generate_password_hash(password)
         services.create_user(user)
-        return Result(msg="注册成功").to_dict(), 201
-
+        return {"data": user.id}, 201
 
 
 class UserApi(Resource):
     """注册"""
+
     def get(self, id):
         user = services.get_user(id)
         if user is None:
-            return Result(), 500
-        return Result(msg="获取成功", data=user.id).to_dict(), 201
-
-
-
+            return {"msg": "未查询到用户信息"}, 404
+        return {"data": user.id}, 200
--- back/app/routes/user_room.py
+++ back/app/routes/user_room.py
@@ -3,7 +3,6 @@ from flask_restful import Resource
 
 from app import services
 from app import entities
-from app.entities.result import Result
 
 
 class UserRoomApi(Resource):
@@ -11,16 +10,18 @@ class UserRoomApi(Resource):
         """获取用户房间映射关系"""
         user_room = services.get_user_room(user_id=user_id, room_id=room_id)
         if user_room is None:
-            return Result(msg="用户不再该房间中").to_dict(), 404
-        return Result(msg="获取成功", data=user_room).to_dict(), 201
+            return {"msg": "用户不再该房间中"}, 404
+        return {"msg": "获取成功", "data": user_room}, 200
 
     def post(self):
         """用户加入房间"""
         user_id = request.get_json()['userId']
         room_id = request.get_json()['roomId']
-        user_room = entities.UserRoom()(room_id=room_id, user_id=user_id)
+        user_room = entities.UserRoom()
+        user_room.room_id=room_id
+        user_room.user_id=user_id
         services.create_user_room(user_room)
-        return Result(msg="注册成功").to_dict(), 201
+        return {"msg": "注册成功"}, 201
 
     def put(self):
         """修改用户在房间中的设置信息"""
@@ -31,10 +32,10 @@ class UserRoomApi(Resource):
 class UserRoomsApi(Resource):
     def get(self):
         """获取用户房间映射关系列表"""
-        user_id = request.get_json()['userId']
-        room_id = request.get_json()['roomId']
+        user_id = request.get_json().get('userId')
+        room_id = request.get_json().get('roomId')
         user_room = services.list_user_room(user_id=user_id, room_id=room_id)
-        return Result(msg="获取成功" ).to_dict(), 201
+        return { "data": user_room }, 200
 
     def delete(self):
         """解散房间或删除用户的连带映射处理"""

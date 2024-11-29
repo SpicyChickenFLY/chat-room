b7cb7a2aec5a326697f50055516c25bec55435c3
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-11-29 16:43:43 +0800

    如何实现最新未读消息和未读消息数目统计

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-
 概念的转变：
 socketio中的join_room步骤，不应该由用户在登陆后手动的去选择，
 而是应该在用户登陆并连接后，由服务端
+
+user(id, name)
+room(id, name)
+user_room_map(user_id, room_id, last_confirm_chat_id)
+chat(id, user_id, room_id, chat_content)
+如何用一句SQL查看指定ID的用户的每个房间的未读聊天消息数据与最新一条消息的内容
+
+```sql
+SELECT
+    urm.room_id,
+    COUNT(c.id) AS unread_messages,
+    latest_chat.id AS latest_chat_id,
+    latest_chat.chat_content AS latest_chat_content
+FROM user_room_map AS urm
+LEFT JOIN chat AS c
+ON urm.room_id = c.room_id
+    AND c.id > urm.last_confirm_chat_id
+LEFT JOIN
+    (
+        SELECT room_id, id, chat_content
+        FROM chat
+        WHERE
+            (room_id, id) IN (
+                SELECT room_id, MAX(id) AS max_id
+                FROM chat
+                GROUP BY room_id
+            )
+    ) AS latest_chat
+ON urm.room_id = latest_chat.room_id
+WHERE urm.user_id = :user_id
+GROUP BY urm.room_id, latest_chat.id, latest_chat.chat_content;
+```
--- front/src/utils/request.ts
+++ front/src/utils/request.ts
@@ -8,7 +8,7 @@ const userStore = useUserStore()
 const baseURL = 'http://127.0.0.1:5000'
 const request = axios.create({
   baseURL: baseURL,
-  timeout: 10000
+  timeout: 10000,
 })
 
 request.interceptors.request.use(
@@ -29,19 +29,18 @@ request.interceptors.response.use(
   (response) => {
     // 判断返回的数据
     const { code, status } = response.data
-    if (code) {
-      if (status === 401) {
-        ElMessage({ type: 'warning', message: 'token超时了' })
-        // 说明token超时了
-        //   await store.dispatch('user/logout') // 调用action 退出登录
-        //  主动跳到登录页
-        router.push('/login') // 跳转到登录页
-      } else {
-        return response.data
-      }
-    } else {
+    console.log(response)
+    if (response.status === 401) {
+      ElMessage({ type: 'warning', message: 'token超时了' })
+      // 说明token超时了
+      //   await store.dispatch('user/logout') // 调用action 退出登录
+      //  主动跳到登录页
+      router.push('/login') // 跳转到登录页
+    } else if (response.status === 500) {
       ElMessage({ type: 'error', message: response.data.message })
       return Promise.reject(response)
+    } else {
+        return response.data
     }
   },
   async (error) => {
